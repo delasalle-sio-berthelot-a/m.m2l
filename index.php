05e92c5a655ba1de11469e6ba4f6cb162ed958bc
delasalle-sio-berthelot-a <delasalle.sio.berthelot.a@gmail.com>
2017-11-28 16:05:06 +0100

    changer mdp

--- index.php
+++ index.php
@@ -74,16 +74,16 @@ switch($action){
 		include_once ('controleurs/CtrlConsulterReservations.php'); break;
 	}
 	case 'ConfirmerReservation': {
-		//include_once ('controleurs/CtrlConfirmerReservation.php'); break;
-		include_once ('controleurs/CtrlMenu.php'); break;	// ligne provisoire
+		include_once ('controleurs/CtrlConfirmerReservation.php'); break;
+		
 	}
 	case 'AnnulerReservation': {
 		//include_once ('controleurs/CtrlAnnulerReservation.php'); break;
 		include_once ('controleurs/CtrlAnnulerReservation.php'); break;	// ligne provisoire
 	}
 	case 'ChangerDeMdp': {
-		//include_once ('controleurs/CtrlChangerDeMdp.php'); break;
-		include_once ('controleurs/CtrlMenu.php'); break;	// ligne provisoire
+		include_once ('controleurs/CtrlChangementMdp.php'); break;
+		
 	}
 	case 'CreerUtilisateur': {
 		include_once ('controleurs/CtrlCreerUtilisateur.php'); break;
