406a30f89f10f1123078b630f569fe0f2a69dc67
delasalle-sio-berthelot-a <delasalle.sio.berthelot.a@gmail.com>
2017-12-12 14:58:02 +0100

    AnnulerResa By Antoine

--- controleurs/CtrlAnnulerReservation.php
+++ controleurs/CtrlAnnulerReservation.php
@@ -1,23 +1,65 @@
-<?php
-// Projet Réservations M2L - version web mobile
-// fichier : controleurs/CtrlConsulterReservations.php
-// Rôle : traiter la demande de consultation des réservations d'un utilisateur
-// écrit par Jim le 12/10/2015
-// modifié par Jim le 28/6/2016
 
-// on vérifie si le demandeur de cette action est bien authentifié
+<?php
+include_once ('modele/DAO.class.php');
+$dao = new DAO();
 if ( $_SESSION['niveauUtilisateur'] != 'utilisateur' && $_SESSION['niveauUtilisateur'] != 'administrateur') {
-    // si le demandeur n'est pas authentifié, il s'agit d'une tentative d'accès frauduleux
-    // dans ce cas, on provoque une redirection vers la page de connexion
-    header ("Location: index.php?action=Deconnecter");
+	// si le demandeur n'est pas authentifié, il s'agit d'une tentative d'accès frauduleux
+	// dans ce cas, on provoque une redirection vers la page de connexion
+	header ("Location: index.php?action=Deconnecter");
 }
 else {
-    // connexion du serveur web à la base MySQL
-    include_once ('modele/DAO.class.php');
-    $dao = new DAO();
-   
-
-    include_once ('vues/VueAnnulerReservation.php');
-    
-    unset($dao);		// fermeture de la connexion à MySQL
+	
+	if ( ! isset ($_POST ["btnAnnulerReservation"]) == true) {
+	
+		$idReservation = '';
+		include_once ('vues/VueAnnulerReservation.php');
+	}
+	
+	else
+	{
+		$idReservation = $_POST ["txtReservation"];
+		$nomUtilisateur = $_SESSION['nom'];
+	
+	
+	// mise à jour de la table mrbs_entry_digicode (si besoin) pour créer les digicodes manquants
+		if (!$dao->existeReservation($idReservation)){
+			$message = "Vous n'êtes pas l'auteur de cette réservation !";
+			$typeMessage = 'avertissement';
+			$themeFooter = $themeNormal;
+			include_once ('vues/VueAnnulerReservation.php');
+		}
+		else {
+			$laReservation = $dao->getReservation($idReservation);
+			$laDateReservation = $laReservation->getEnd_time();
+			
+			if ($laDateReservation <= time()){
+				$message = "Cette réservation est déjà passée !";
+				$typeMessage = 'avertissement';
+				$themeFooter = $themeNormal;
+				include_once ('vues/VueAnnulerReservation.php');
+			}
+			else {
+				// On teste si l'utilisateur est le créateur de la réservation
+				if ( !$dao->estLeCreateur($nomUtilisateur,$idReservation)){
+					$message = "Annulation impossible, vous n'êtes pas le créateur de la réservation.";
+					$typeMessage = 'avertissement';
+					$themeFooter = $themeNormal;
+					include_once ('vues/VueAnnulerReservation.php');
+				}
+			
+				else {
+					// Si la réservation existe et a été faite par l'utilisateur elle est annulée
+					$ok = $dao->annulerReservation($idReservation);
+			
+				if ($ok) {
+						$message = 'Réservation correctement annulée.';
+						$typeMessage = 'information';
+						$themeFooter = $themeNormal;
+						include_once ('vues/VueAnnulerReservation.php');
+					}
+				}
+			}
+			
+			}
+			}
 }
--- vues/VueAnnulerReservation.php
+++ vues/VueAnnulerReservation.php
@@ -1,36 +1,60 @@
+
 <?php
-// Projet Réservations M2L - version web mobile
-// fichier : vues/VueConsulterReservations.php
-// Rôle : visualiser la liste des réservations à venir d'un utilisateur
-// cette vue est appelée par le contôleur controleurs/CtrlConsulterReservations.php
-// Création : 12/10/2015 par JM CARTRON
-// Mise à jour : 31/5/2016 par JM CARTRON
+	// Projet Réservations M2L - version web mobile
+	// fichier : vues/VueConsulterReservations.php
+	// Rôle : visualiser la liste des réservations à venir d'un utilisateur
+	// cette vue est appelée par le contôleur controleurs/CtrlConsulterReservations.php
+	
 ?>
 <!doctype html>
 <html>
 	<head>
 		<?php include_once ('vues/head.php'); ?>
+	<script>
+		$(document).bind('pageinit', function() {
+			<?php if ($typeMessage != '') { ?>
+			
+				// affiche la boîte de dialogue 'affichage_message'
+				$.mobile.changePage('#affichage_message', {transition: "<?php echo $transition; ?>"});
+			<?php } ?>		
+		} ) ;
+		</script>
+		
 	</head>
-	 
+	
 	<body>
-		<div data-role="page">
+		<div data-role="page" id="page_principale">
 			<div data-role="header" data-theme="<?php echo $themeNormal; ?>">
 				<h4>M2L-GRR</h4>
 				<a href="index.php?action=Menu" data-transition="<?php echo $transition; ?>">Retour menu</a>
 			</div>
+			
 			<div data-role="content">
 				<h4 style="text-align: center; margin-top: 0px; margin-bottom: 0px;">Annuler une réservation</h4>
-				
-			
-				<input type="text" name="txtNom" id="txtNom" data-mini="true" placeholder="Entrez le numéro de reservation">
-		
-				<input type="submit" name="btnConnecter" id="btnConnecter" data-mini="true" data-ajax="false" value="Annuler la réservation">
+				<form action="index.php?action=AnnulerReservation" method="post" data-ajax="false">
+					<div data-role="fieldcontain">
+						<input type="text" name="txtReservation" id="txtReservation" required placeholder="Entrez le numéro de réservation" value="<?php echo $idReservation ?>">
+					</div>
+					<div data-role="fieldcontain">
+						<input type="submit" name="btnAnnulerReservation" id="btnAnnulerReservation" value="Annuler la réservation" data-mini="true">
+					</div>
+				</form>
+
+				<?php if($debug == true) {
+					// en mise au point, on peut afficher certaines variables dans la page
+					echo "<p>name = " . $name . "</p>";
+					echo "<p>adrMail = " . $adrMail . "</p>";
+					echo "<p>level = " . $level . "</p>";
+				} ?>
 				
 			</div>
-			<div data-role="footer" data-position="fixed" data-theme="<?php echo $themeNormal;?>">
+			
+			<div data-role="footer" data-position="fixed" data-theme="<?php echo $themeNormal; ?>">
 				<h4>Suivi des réservations de salles<br>Maison des ligues de Lorraine (M2L)</h4>
 			</div>
 		</div>
+		
+		<?php include_once ('vues/dialog_message.php'); ?>
 		
 	</body>
 </html>
